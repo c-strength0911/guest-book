341e580940d3e226df46e594e7f34b0fb312ee30
c-strength0911 <57119447+c-strength0911@users.noreply.github.com>
2021-01-04 16:31:36 +0900

    01/04

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
-const mysql = require("mysql");
+// const mysql = require("mysql");
 const cors = require("cors");
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "1234",
-  database: "test_db",
-});
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "1234",
+//   database: "test_db",
+// });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use("/static", express.static("public"));
+app.use(express.static("public"));
 app.use(cors());
 
-connection.connect((err) => {
-  if (err) {
-    console.error(err.stack);
-  } else {
-    console.log("Connected to Mysql");
-  }
+// connection.connect((err) => {
+//   if (err) {
+//     console.error(err.stack);
+//   } else {
+//     console.log("Connected to Mysql");
+//   }
+// });
+app.get("/test", function (req, res) {
+  res.json("test");
 });
-
 app.get("/join", function (req, res) {
-  var sql = "SELECT * FROM guestbook ORDER BY id DESC";
+  // var sql = "SELECT * FROM guestbook ORDER BY id DESC";
   connection.query(sql, function (err, rows, fields) {
     if (err) console.log("query is not excuted. select fail...\n" + err);
     else return res.json(rows);
@@ -33,16 +35,18 @@ app.get("/join", function (req, res) {
 });
 
 app.post("/insert", function (req, res) {
-  let insertQuery = "INSERT INTO guestbook(name, content) VALUE(?,?)";
+  // let insertQuery = "INSERT INTO guestbook(name, content) VALUE(?,?)";
   const { name, content } = req.body;
-  connection.query(insertQuery, [name, content], (err) => {
-    if (err) {
-      console.log(err);
-      return res.status(400).json({ result: "success" });
-    } else {
-      return res.status(200).json({ result: "success" });
-    }
-  });
+
+  res.send(name, content);
+  // connection.query(insertQuery, [name, content], (err) => {
+  //   if (err) {
+  //     console.log(err);
+  //     return res.status(400).json({ result: "fail" });
+  //   } else {
+  //     return res.status(200).json({ result: "success" });
+  //   }
+  // });
 });
 
 app.get("comment/join", function (req, res) {});
