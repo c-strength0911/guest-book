beea40e911eb5f914a0c0d30e12cbddc183652e6
c-strength0911 <57119447+c-strength0911@users.noreply.github.com>
2021-01-06 15:33:29 +0900

    eidt/delete  api 추가

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const morgan = require("morgan");
 const app = express();
 const mysql = require("mysql");
 const cors = require("cors");
+
+const PORT = 8000;
+
 const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -25,23 +28,20 @@ connection.connect((err) => {
   }
 });
 
-function isCorrect(req, res, next) {
-  const { name, content } = req.body;
-  if (!name.trim() || !content.trim()) {
-    return () => {
-      res.status(400).json({
-        isSuccess: false,
-        message: "작성자 이름과 내용을 입력해 주세요",
-      });
-      next();
-    };
-  } else next();
-}
+// function isCorrect(req, res, next) {
+//   const { name, content } = req.body;
+//   if (!name.trim() || !content.trim()) {
+//     return () => {
+//       res.status(400).end();
+//       next();
+//     };
+//   } else next();
+// }
 
-app.get("/test", function (req, res) {
+app.get("/test", (req, res) => {
   res.json("test");
 });
-app.get("/join", function (req, res) {
+app.get("/join", (req, res) => {
   var sql = "SELECT * FROM guestbook ORDER BY id DESC";
   connection.query(sql, function (err, rows, fields) {
     if (err) console.log("query is not excuted. select fail...\n" + err);
@@ -49,7 +49,7 @@ app.get("/join", function (req, res) {
   });
 });
 
-app.post("/insert", isCorrect, function (req, res) {
+app.post("/insert", (req, res) => {
   let insertQuery = "INSERT INTO guestbook(name, content) VALUE(?,?)";
   const { name, content } = req.body;
   connection.query(insertQuery, [name, content], (err) => {
@@ -61,10 +61,23 @@ app.post("/insert", isCorrect, function (req, res) {
     }
   });
 });
-app.post("/edit", isCorrect, (req, res) => {
-  let insertQuery = "INSERT INTO guestbook(name, content) VALUE(?,?)";
-  const { name, content } = req.body;
-  connection.query(insertQuery, [name, content], (err) => {
+
+app.post("/delete", (req, res) => {
+  let deleteQuery = "DELETE FROM guestbook WHERE id = (?)";
+  const { id } = req.body;
+  connection.query(deleteQuery, id, (err) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ result: "fail" });
+    } else {
+      return res.status(200).json({ result: "success" });
+    }
+  });
+});
+app.post("/edit", (req, res) => {
+  const { content, id } = req.body;
+  let editQuery = "UPDATE guestbook SET content = (?) WHERE id = (?)";
+  connection.query(editQuery, [content, id], (err) => {
     if (err) {
       console.log(err);
       return res.status(500).json({ result: "fail" });
@@ -77,6 +90,6 @@ app.post("/edit", isCorrect, (req, res) => {
 app.get("comment/join", function (req, res) {});
 app.post("comment/insert", function (req, res) {});
 // app.post("/create", (req, res) => {});
-app.listen(8000, () => {
-  console.log("OPEN");
+app.listen(PORT, () => {
+  console.log(`OPENED PORT : ${PORT}`);
 });
